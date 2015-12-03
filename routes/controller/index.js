148b5c733c9ed447b1d377785938217e14edfede
zhou-yg <zhou.y.g890@gmail.com>
2015-12-03 21:50:59 +0800

    加入env

--- routes/controller/index.js
+++ routes/controller/index.js
@@ -1,8 +1,12 @@
 /**
  * Created by zyg on 15/11/6.
  */
+var env = process.env.NODE_ENV;
+
 module.exports = {
   index:function(req,res){
-    res.render('index');
+    res.render('index',{
+      env:env
+    });
   }
 };
